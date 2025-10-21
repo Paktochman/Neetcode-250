254c1c235474c26feba7f1ea108f08879f1f47b3
Priyanka Katoch <priyankakatoch@Priyankas-MacBook-Air.local>
2025-10-21 21:13:38 +0530

    Add solution for Valid Palindrome II (delete at most one char, O(n) time, O(1) space)

---
+++ ValidPalindromeII.java
@@ -0,0 +1,30 @@
+public class ValidPalindromeII {
+    
+    public static boolean validPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) == s.charAt(right)) {
+                left++;
+                right--;
+            } else {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+        }
+        return true;
+    }
+    
+    private static boolean isPalindrome(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(validPalindrome("aca"));      // true
+        System.out.println(validPalindrome("abbadc"));   // false
+    }
+}
+
