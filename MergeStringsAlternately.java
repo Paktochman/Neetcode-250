bb6c6556a0172d53645a6b4006a7b3f817bc63a8
Priyanka Katoch <priyankakatoch@Priyankas-MacBook-Air.local>
2025-10-21 21:13:01 +0530

    Add solution for Merge Strings Alternately (O(n) time, O(1) extra space)

---
+++ MergeStringsAlternately.java
@@ -0,0 +1,18 @@
+public class MergeStringsAlternately {
+    public static String mergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0, j = 0;
+        while (i < word1.length() && j < word2.length()) {
+            sb.append(word1.charAt(i++));
+            sb.append(word2.charAt(j++));
+        }
+        while (i < word1.length()) sb.append(word1.charAt(i++));
+        while (j < word2.length()) sb.append(word2.charAt(j++));
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(mergeAlternately("abc", "xyz")); // axbycz
+        System.out.println(mergeAlternately("ab", "abbxxc")); // aabbbxxc
+    }
+}
