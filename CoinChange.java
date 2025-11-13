8e03792512aa51f8d291cebe5b65153b1c62c64b
Priyanka Katoch <priyankakatoch@Priyankas-MacBook-Air.local>
2025-11-13 21:04:49 +0530

    Add Neetcode-250 problems: Palindromic Substrings, Decode Ways, Coin Change, Maximum Product Subarray, and Word Break

---
+++ CoinChange.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class CoinChange {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new CoinChange().coinChange(new int[]{1, 5, 10}, 12)); // 3
+    }
+}
---
+++ MaximumProductSubarray.java
@@ -0,0 +1,20 @@
+public class MaximumProductSubarray {
+    public int maxProduct(int[] nums) {
+        int max = nums[0], min = nums[0], ans = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                int temp = max;
+                max = min;
+                min = temp;
+            }
+            max = Math.max(nums[i], max * nums[i]);
+            min = Math.min(nums[i], min * nums[i]);
+            ans = Math.max(ans, max);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new MaximumProductSubarray().maxProduct(new int[]{1, 2, -3, 4})); // 4
+    }
+}
